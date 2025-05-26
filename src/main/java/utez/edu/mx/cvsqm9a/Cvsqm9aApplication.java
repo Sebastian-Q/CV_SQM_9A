714a95650acbc8bf61515c14ba6ba0e961d03783
Sebastian-Q <20203tn049@utez.edu.nx>
2025-05-26 17:44:11 -0600

    [FIRST COMMIT] Subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cvsqm9a/Cvsqm9aApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvsqm9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvsqm9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvsqm9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvsqm9a/Cvsqm9aApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvsqm9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvsqm9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
